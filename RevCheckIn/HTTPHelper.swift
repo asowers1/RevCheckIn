90af5c5e8579909fff47cb87460afe0334d78fdd
asow123@gmail.com <asow123@gmail.com>
2014-08-27 10:18:44 -0400

    added commenting in getAllUsers

--- RevCheckIn/HTTPHelper.swift
+++ RevCheckIn/HTTPHelper.swift
@@ -142,9 +142,17 @@ class HTTPHelper: NSObject {
         var request = HTTPTask()
         request.GET("http://experiencepush.com/rev/rest/index.php", parameters: params, success: {(response: HTTPResponse) -> Void in
             let jsonObject : AnyObject! = NSJSONSerialization.JSONObjectWithData(response.responseObject! as NSData, options: NSJSONReadingOptions.MutableContainers, error: nil)
+            
+            // remove all users here:
+            
+            // iterate through users in JSON
             if let ar: [AnyObject] = jsonObject as? [AnyObject]{
                 for user in ar{
+                    //get all memembers:
                     let business_name: AnyObject! = ar[0]["business_name"]
+                    
+                    
+                    // add each user per iteration
                     
                 }
             }
